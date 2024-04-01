ac06b9682629f180368d677a2f30a7579707f48c
kristiinlee <kristiinlee@gmail.com>
2024-03-31 20:35:04 -0400

    Added github repo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by <a href="https://github.com/kristiinlee" target="_blank" rel="noreferrer">Kristin Jones</a> and is open sourced on <a href="https://github.com/kristiinlee/kristins-react-weatherapp" target="_blank" rel="noreferrer">GitHub</a>
+      </footer>
     </div>
   );
 }
